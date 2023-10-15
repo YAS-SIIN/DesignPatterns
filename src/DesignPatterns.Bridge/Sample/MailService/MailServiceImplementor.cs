cfa8a5ce0dbab8000ae5a2103d5529104419fd2c
Yasin Asadnezhad <yasin.asadnezhad@msn.com>
2023-10-15 16:00:27 -0400

    Fixed a bug.

--- src/DesignPatterns.Bridge/Sample/MailService/MailServiceImplementor.cs
+++ src/DesignPatterns.Bridge/Sample/MailService/MailServiceImplementor.cs
@@ -6,37 +6,37 @@ using System.Threading.Tasks;
 
 namespace BridgeSample.MailService;
 
-    public interface IMailServiceImplementor
+public interface IMailServiceImplementor
+{
+    void SendEmail(string Reciver, string Title, string Message);
+}
+
+public class GmailService : IMailServiceImplementor
+{
+    public void SendEmail(string Reciver, string Title, string Message)
     {
-        void SendEmail(string Reciver, string Title, string Message);
+        //Send email
+        Console.WriteLine($"Email --{Title}-- Sended to --{Reciver}--  With   **{nameof(GmailService)}**");
+    }
+}
+
+public class YahooService : IMailServiceImplementor
+{
+    public void SendEmail(string Reciver, string Title, string Message)
+    {
+        //Send email
+        Console.WriteLine($"Email --{Title}-- Sended to --{Reciver}-- With    **{nameof(YahooService)}**");
+    }
+}
+
+
+public class MyCompanyMailService : IMailServiceImplementor
+{
+    public void SendEmail(string Reciver, string Title, string Message)
+    {
+
+        Console.WriteLine($"Email --{Title}-- Sended to --{Reciver}-- With    **{nameof(MyCompanyMailService)}**");
     }
 
-    public class GmailService : IMailServiceImplementor
-    {
-        public void SendEmail(string Reciver, string Title, string Message)
-        {
-            //Send email
-            Console.WriteLine($"Email --{Title}-- Sended to --{Reciver}--  With   **{nameof(GmailService)}**");
-        }
-    }
-
-    public class YahooService : IMailServiceImplementor
-    {
-        public void SendEmail(string Reciver, string Title, string Message)
-        {
-            //Send email
-            Console.WriteLine($"Email --{Title}-- Sended to --{Reciver}-- With    **{nameof(YahooService)}**");
-        }
-    }
-
-
-    public class MyCompanyMailService : IMailServiceImplementor
-    {
-        public void SendEmail(string Reciver, string Title, string Message)
-        {
-            {
-
-                Console.WriteLine($"Email --{Title}-- Sended to --{Reciver}-- With    **{nameof(MyCompanyMailService)}**");
-        }
-    }
+}
 
