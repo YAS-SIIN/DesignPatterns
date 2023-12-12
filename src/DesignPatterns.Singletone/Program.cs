d10f2581b8e215a2328f72a338317ed05c3f33fd
Yasin Asadnezhad <yasin.asadnezhad@msn.com>
2023-12-12 09:05:00 +0330

    Coded singletone pattern.

---
+++ src/DesignPatterns.Singletone/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using DesignPatterns.Singletone;
+
+Console.WriteLine("Hello, World!");
+
+
+var singleTone1 = Singelton.GetInstance();
+var singleTone2 = Singelton.GetInstance();
+
+
+Console.ReadLine();
---
+++ src/DesignPatterns.Singletone/Singelton.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Singletone;
+
+public class Singelton
+{
+    public static Singelton uniqeSingelton;
+    private Singelton() {
+    
+    }
+
+    public static Singelton GetInstance()
+    {
+        if (uniqeSingelton is null)
+            uniqeSingelton = new();
+        
+        return uniqeSingelton;
+    }
+}
