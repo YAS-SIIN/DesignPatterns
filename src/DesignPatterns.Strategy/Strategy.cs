da9678b38650562dd7894d6def1afa226a6a720f
Yasin Asadnezhad <yasin.asadnezhad@msn.com>
2024-01-22 15:23:46 +0330

    Coded structure of strategy pattern. Changed on folders path in solution.

---
+++ src/DesignPatterns.Strategy/Strategy.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Strategy;
+
+public abstract class Strategy
+{
+    public abstract void AlgoritmInterface();
+}
